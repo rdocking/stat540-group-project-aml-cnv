f1a607d41cabed11266e59c185d7be203980915e
Rod Docking <rdocking@bcgsc.ca>
2014-03-20 09:50:11 -0700

    Switch list of column name changes to a list of tuples

--- data/parse_supplementary_table.py
+++ data/parse_supplementary_table.py
@@ -8,15 +8,15 @@ Created by Rod Docking on 2014-03-19.
 
 import csv
 
-def standardize_nomenclature(row, original_names, modified_names):
+def standardize_nomenclature(row, column_names):
     """
     Standardize and simplify nomenclature
     """
     
     # Set up a new dict with simplified column names
     tidied_row = {}
-    for old, new in zip(original_names, modified_names):
-        tidied_row[new] = row.pop(old)
+    for original, modified in column_names:
+        tidied_row[modified] = row.pop(original)
 
     # Simplify row entries
     tidied_row['RNAseq_available'] = 'TRUE' if \
@@ -31,23 +31,31 @@ def main():
     """
     
     # Simplify variable names
-    original_names = ['TCGA Patient ID', 'RNAseq data?', 'Expired?  4.30.13',
-                      'Sex', 'Race', 'FAB', 'Age', '%BM Blast', 'WBC',
-                      '%PB Blast', 'Subclones deteceted by WGS', 
-                      'Cytogenetics', 'Gene Fusions by RNA-Seq',
-                      'Inferred genomic rearrangement (from RNA-Seq fusion)',
-                      'Cytogenetic Classification', 'RISK (Cyto)',
-                      'Molecular Classification', 'RISK (Molecular)',
-                      'SVs (from WGS)']
-    modified_names = ['TCGA_patient_id', 'RNAseq_available', 'Expired',
-                      'Sex', 'Race', 'FAB', 'Age', 'BM_blast_pct',
-                      'White_blood_cell_count', 'PB_blast_pct',
-                      'WGS_subclones_detected', 'Cytogenetics',
-                      'RNAseq_gene_fusions', 
-                      'RNAseq_inferred_genomic_rearrangement',
-                      'Cytogenetic_classification', 'Cytogenetic_risk',
-                      'Molecular_classification', 'Molecular_risk',
-                      'SVs_from_WGS']
+    # This is a list of tuples, with each member consisting of the 
+    # original and revised variable names
+    column_names = [
+        ('TCGA Patient ID', 'TCGA_patient_id'),
+        ('RNAseq data?', 'RNAseq_available'),
+        ('Expired?  4.30.13', 'Expired'),
+        ('Sex', 'Sex'), 
+        ('Race', 'Race'),
+        ('FAB', 'FAB_subtype'),
+        ('Age', 'Age'),
+        ('%BM Blast', 'BM_blast_pct'),
+        ('WBC', 'White_blood_cell_count'),
+        ('%PB Blast', 'PB_blast_pct'),
+        ('Subclones deteceted by WGS', 'WGS_subclones_detected'),
+        ('Cytogenetics', 'Cytogenetics'),
+        ('Gene Fusions by RNA-Seq', 'RNAseq_gene_fusions'),
+        ('Inferred genomic rearrangement (from RNA-Seq fusion)',
+         'RNAseq_inferred_genomic_rearrangement'),
+        ('Cytogenetic Classification', 'Cytogenetic_classification'),
+        ('RISK (Cyto)', 'Cytogenetic_risk'),
+        ('Molecular Classification', 'Molecular_classification'),
+        ('RISK (Molecular)', 'Molecular_risk'),
+        ('SVs (from WGS)', 'SVs_from_WGS')
+    ]
+    modified_names = [t[1] for t in column_names]
 
     # Open the CSV file for reading
     supp_csv_file = "SuppTable01.update.2013.05.13.csv"
@@ -60,8 +68,7 @@ def main():
                             fieldnames=modified_names)
         csv_writer.writeheader()
         for row in csv_reader:
-            tidied_row = standardize_nomenclature(row, original_names, 
-                                                  modified_names)
+            tidied_row = standardize_nomenclature(row, column_names)
             if tidied_row['TCGA_patient_id']:
                 csv_writer.writerow(tidied_row)
 
